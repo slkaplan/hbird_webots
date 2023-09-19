5441d688e216f916faa04e25cfff0fbddc63bae5
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2023-09-19 02:54:00 -0400

    Add theoretically complete but untested controller

--- hbird_webots/scripts/controller.py
+++ hbird_webots/scripts/controller.py
@@ -56,14 +56,41 @@ class Controller3D():
         """
         U = np.array([0.,0.,0.,0.])
 
-        e = np.array([
-            setpoint.position.z - state.position.z,
-        ])
+        # Position controller (cascades into attitude controller)
+        e_z = setpoint.position.z - state.position.z
+        e_x = setpoint.position.x - state.position.x
+        e_y = setpoint.position.y - state.position.y
 
-        e_dot = np.array([
-            0 - state.velocity.z
-        ])
+        e_dot_z = 0 - state.velocity.z
+        e_dot_x = 0 - state.velocity.x
+        e_dot_y = 0 - state.velocity.y
 
-        U[0] = self.params.mass * (self.kp_z * e[0] + self.kd_z * e_dot[0] + self.params.g)
+        z_dotdot_c = self.kp_z * e_z + self.kd_z * e_dot_z
+        x_dotdot_c = self.kp_x * e_x + self.kd_x * e_dot_x
+        y_dotdot_c = self.kp_y * e_y + self.kd_y * e_dot_y
+
+        s_psi = np.sin(setpoint.heading)
+        c_psi = np.cos(setpoint.heading)
+
+        U[0] = self.params.mass * (z_dotdot_c + self.params.g)
+        phi_d = (1 / self.params.g) * (x_dotdot_c * s_psi - y_dotdot_c * c_psi)
+        theta_d = (1 / self.params.g) * (x_dotdot_c * c_psi - y_dotdot_c * s_psi)
+
+        # TOOD: compute angular rate error
+
+        # Attitude controller
+        # FIXME: not sure how phi/theta correspond to roll and pitch;
+        # orientation.x, y, and z are yaw, pitch and roll, respectively (see hbird_sim_node.py)
+        e_phi = phi_d - state.orientation.x
+        e_theta = theta_d - state.orientation.y
+        e_psi = setpoint.heading - state.orientation.z
+
+        e_dot_phi = 0 - state.angular_velocity.x
+        e_dot_theta = 0 - state.angular_velocity.y
+        e_dot_psi = 0 - state.angular_velocity.z
+
+        U[1] = self.kp_phi * e_phi + self.kd_p * e_dot_phi
+        U[2] = self.kp_theta * e_theta +  self.kd_q * e_dot_theta
+        U[3] = self.kp_psi * e_psi + self.kd_r * e_dot_psi
 
         return U
